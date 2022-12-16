7b7f546b69b9732f16b84349eddf053827f393e4
muluken-beep <60249295+muluken-beep@users.noreply.github.com>
2022-12-15 16:44:22 -0800

    Adding Models and controllers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 
 builder.Services.AddControllersWithViews(); 
 
