65f1401e0c0b282b2fab484958c5f41b4b22fb87
muluken-beep <60249295+muluken-beep@users.noreply.github.com>
2022-12-21 06:48:16 -0800

    Using entity framework to create database

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
 
-builder.Services.AddControllersWithViews(); 
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
+
 
 var app = builder.Build();
 
@@ -18,5 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
+
+DbInitializer.Seed(app);
 
 app.Run();
